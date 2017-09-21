b16383513ff67e124e759560d9823b4c96523934
Ömer Cinbat <ocinbat@outlook.com>
2017-09-21 21:35:34 +0300

    Added tests.

---
+++ Test/JokesControllerTests.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Joker.Controllers;
+using Joker.Data;
+using Joker.Objects;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using Ploeh.AutoFixture;
+using Xunit;
+
+namespace Test
+{
+    public class JokesControllerTests
+    {
+        [Fact]
+        public void QueryJokes_Should_Return_List_Of_Jokes()
+        {
+        }
+    }
+}
